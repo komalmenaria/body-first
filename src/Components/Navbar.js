7e090139dbd79cba99b747d1f0147a50cfcc1741
komal menaria <komalmenariacpm2003@gmail.com>
2022-08-25 11:55:50 +0530

    logo added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom";
-
+import Bodyfirstlogo from "../Images/Body-First-Logo.png"
 
 
 function Navbar() {
@@ -8,7 +8,7 @@ function Navbar() {
     <>
     <nav className="navbar navbar-dark navbar-expand-lg bg-dark body-first-navbar">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar</Link>
+    <Link className="navbar-brand main-logo" to="/"> <img src={Bodyfirstlogo} alt="" /> </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
