34a776896608b65b761c25f0406cb99c9bcc555a
tonudon86 <tonudon86@gmail.com>
2022-08-25 11:05:53 +0530

    latancy fix in onbording action

--- src/actions/onboardingAction.js
+++ src/actions/onboardingAction.js
@@ -2,25 +2,26 @@ import axios from "axios"
 
 import Config from "../Config"
 import { ASSIGN_AGE_AND_GENDER } from '../Constant'
-let user = localStorage.getItem('user')
-let token
-if (user) {
-    user = JSON.parse(user)
-    token = user.token
-}
-let config = {
-    headers: {
-        token
-    }
-}
+
 export const assingAgeAndGender = (body) => async (dispatch) => {
+    let user = localStorage.getItem('user')
+    let token
+    if (user) {
+        user = JSON.parse(user)
+        token = user.token
+    }
+    let config = {
+        headers: {
+            token
+        }
+    }
     return new Promise(async (resolve, resject) => {
         console.log(config)
         try {
             dispatch({
                 type: ASSIGN_AGE_AND_GENDER + "REQUEST"
             })
-            await axios.post(`${Config.BASE_URL}${Config.ASSIGN_AGE_AND_GENDER}`, body,config)
+            await axios.post(`${Config.BASE_URL}${Config.ASSIGN_AGE_AND_GENDER}`, body, config)
             dispatch({
                 type: ASSIGN_AGE_AND_GENDER + "SUCCESS",
                 payload: body
