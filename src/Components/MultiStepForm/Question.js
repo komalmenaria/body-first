4905a4d2d98f81518a9affaeaf7e9b5b54f86a1a
komal menaria <komalmenariacpm2003@gmail.com>
2022-08-28 11:46:19 +0530

    button added

--- src/Components/MultiStepForm/Question.js
+++ src/Components/MultiStepForm/Question.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Question() {
   return (
     <>
-        <form action="#">
-<center>What are you looking for?</center>
+      <form action="#">
+        <center>What are you looking for?</center>
 
-<div className='option-fields'>
-<div className='single-ans-filed'>
-<input type="radio" id="html" name="fav_language" value="HTML" />
-<label for="html">HTML</label>
-</div>
-<div className='single-ans-filed'>
-<input type="radio" id="css" name="fav_language" value="CSS" />
-<label for="css">CSS</label>
-</div>
+        <div className="option-fields">
+          <div className="single-ans-filed">
+            <input type="radio" id="html" name="fav_language" value="HTML" />
+            <label for="html">HTML</label>
+          </div>
+          <div className="single-ans-filed">
+            <input type="radio" id="css" name="fav_language" value="CSS" />
+            <label for="css">CSS</label>
+          </div>
+        </div>
 
-</div>
-    </form>
+      </form>
+      
+      <div className="next-pre-button-for-form">
+          <button type="button">Previous</button>
+          <button type="button">Next</button>
+
+        </div>
     </>
-  )
+  );
 }
 
-export default Question
+export default Question;
